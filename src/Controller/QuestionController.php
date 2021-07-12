127b93c4a489b74ff583471db95d042bf8bd5340
Shubham <shubham4315@gmail.com>
2021-07-13 00:13:36 +0200

    Simple Update on click

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Question;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class QuestionController extends AbstractController
 {
@@ -52,6 +53,8 @@ class QuestionController extends AbstractController
                 $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
             }
 
+            $question->setVotes(rand(-20,50));
+
             $entityManager->persist($question);
             $entityManager->flush();
 
@@ -84,5 +87,21 @@ class QuestionController extends AbstractController
             'question' => $question,
             'answers' => $answers,
         ]);
+    }
+
+    /**
+     * @Route("/questions/{slug}/vote", name="app_question_vote", methods="POST")
+     */
+    public function questionVote(Question $question,Request $request)
+    {
+        $direction = $request->request->get('direction');
+        if($direction==='up'){
+            $question->setVotes($question->getVotes()+1);
+        }
+        if($direction==='down'){
+            $question->setVotes($question->getVotes()-1);
+        }
+
+        dd($question);
     }
 }
